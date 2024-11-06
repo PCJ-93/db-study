3ffc22633c537aa0b29f05a4265991d2014646eb
PCJ-93 <pw2900@naver.com>
2024-11-06 16:57:27 +0900

    복수행 함수 , 모닝퀴즈

--- select03.sql
+++ select03.sql
@@ -138,6 +138,201 @@ ORDER BY deptno;
 -- 연습 끝 --
 
 
+--- 모닝 퀴즈 ---
+/*emp2 테이블을 기준으로 아래 정보를 출력
+사원들 중에 70년대 생이면서 지역번호를 기준으로 서울(02), 경기도(031)에 거주하는
+사원들의 정보를 아래와 같이 출력하시오.
+사번, 이름, 생년월일, 취미, 급여(pay), 성과급(급여의 150%)
+, 직원분류(단, 가족과 같은 직원이라서 family로 표기)
+,전화번호, 급여수준
+(단, 급여수준은 아래와 같이 분류)
+3500만 1원 ~ 4500만 : '하'
+4500만 1원 ~ 6천만 : '중"
+6000만 1원 이상 : '상'
+그 외... : '화이팅'
+*/
+SELECT
+    empno 사번,
+    name 이름,
+    birthday 생년월일,
+    hobby 취미,
+    pay 급여,
+    pay*1.5 성과급,
+    REPLACE(emp_type,'employee', 'family') 직원분류,
+    --SUBSTR(emp_type, 1, INSTR(emp_type,' '))||'family' 직원분류,
+    tel,
+    CASE
+        WHEN pay BETWEEN 35000001 AND 45000000 THEN '하'
+        WHEN pay BETWEEN 45000001 AND 60000000 THEN '중'
+        WHEN pay >60000000 THEN '상'
+        ELSE '화이팅'
+        END 급여수준
+FROM emp2
+WHERE 
+    SUBSTR(TO_CHAR(birthday, 'YY'),1,1) = '7'
+    AND
+    SUBSTR(tel,1,(INSTR(tel,')')-1)) IN('02','031');
+
+--- 모닝퀴즈 끝 ---
+
+-- HAVING 절 --
+SELECT... 4
+FROM.. 1
+WHERE... 2
+GROUP BY.. 3
+ORDER BY.. 5
+
+SELECT... 5
+FROM.. 1
+WHERE... 2
+GROUP BY.. 3
+HAVING..4
+ORDER BY.. 6
+
+SELECT 
+    deptno,
+    AVG(sal)
+FROM emp
+--WHERE AVG(sal) >= 2000
+GROUP BY deptno
+HAVING AVG(sal)>=2000;
+
+1) 급여가 1500 이상인 직원들만 대상으로, 부서별 평균 급여
+SELECT 
+    deptno,
+    AVG(sal)
+FROM emp
+WHERE sal >= 1500
+GROUP BY deptno;
+
+2) 부서별 평균 급여가 1500 이상인 경우만, 부서별 평균 급여 보여주기
+SELECT 
+    deptno,
+    AVG(sal)
+FROM emp
+GROUP BY deptno
+HAVING AVG(sal) >= 1500;
+
+각 학년별 평균 몸무게
+평균몸무게 65키로 이상인 경우만 확인
+SELECT
+    grade,
+    AVG(weight)
+FROM student
+GROUP BY grade
+HAVING AVG(weight)>=65;
+
+4학년을 제외하고
+각 학년별 평균 몸무게
+평균 몸무게 65키로 이상인 경우만 확인
+SELECT
+    grade,
+    AVG(weight)
+FROM student
+WHERE grade <> 4
+GROUP BY grade
+HAVING AVG(weight)>=65;
+
+SELECT
+    grade,
+    AVG(weight)
+FROM student
+GROUP BY grade
+HAVING grade != 4 AND AVG(weight)>=65;
 
 
+-- ROLLUP() 함수
+SELECT
+    deptno,
+    job,
+    ROUND(AVG(sal),2)
+FROM emp
+GROUP BY deptno, job
+ORDER BY deptno;
+
+SELECT
+    deptno,
+    job,
+    ROUND(AVG(sal),2)
+FROM emp
+GROUP BY ROLLUP(deptno,job);
+
+
+GROUP BY ROLLUP(deptno,job);  --뒤에서부터 하나씩 없애면서 그룹화
+1) deptno, job 그룹화 모은다
+2) deptno 그룹화 => 계
+3) () 그룹화 => 계  -- 리스트 전체 의 계 2)번의 계 아님
+SELECT
+    AVG(1300+2450+5000+800+3000+2975+950+2850+1400)
+FROM dual;
+
+GROUP BY ROLLUP(deptno,job,mgr);
+1) deptno, job, mgr 그룹
+2) deptno, job 계
+3) deptno 계
+4) () 계
+
+SELECT 
+    deptno,
+    job,
+    mgr,
+    ROUND(AVG(sal),1)
+FROM emp
+GROUP BY ROLLUP(deptno,job,mgr);
+
+
+--GROUP BY ROLLUP ( (deptno,job) );
+1) (deptno,job) 그룹화
+2) () 계
+SELECT
+    deptno,
+    job,
+    ROUND(AVG(sal),1)
+FROM emp
+GROUP BY ROLLUP ( (deptno,job) );
+
+--GROUP BY ROLLUP ( deptno,(job,mgr) );
+1) deptno, job, mgr 그룹
+2) deptno 계
+3) () 계
+SELECT
+    deptno,
+    job,
+    mgr,
+    ROUND(AVG(sal),1)
+FROM emp
+GROUP BY ROLLUP ( deptno,(job,mgr) );
+
+-- CUBE --  ROLLUP 이랑 비슷한데 모든 계를 출력해줌
+SELECT
+    deptno,
+    job,
+    mgr,
+    ROUND(AVG(sal),1)
+FROM emp
+GROUP BY CUBE ( deptno,job,mgr );
+
+
+-- RANK(), DENSE_RANK() 함수  일반적으로 OVER랑 같이 사용
+순위 RANK() OVER (ORDER BY 정렬기준)
+그룹단위로 순위 RANK() OVER (PARTITION BY 기준 ORDER BY 정렬기준)
+                         --group by   PARTITION BY ~~ 그룹지은 기준에서 순위 매김
+
+SELECT 
+    ename,
+    sal,
+    RANK() OVER (ORDER BY sal DESC),  -- 같은등수이후 같은등수도 순서로치고 다음순서로 넘어감 7 8 9 9 11 12..
+    DENSE_RANK() OVER (ORDER BY sal DESC) -- 같은 등수이후 순서에맞게 정리 7 8 9 9 10 11 ..
+FROM emp;
+
+SELECT 
+    name,
+    height,
+--    RANK() OVER (ORDER BY height DESC) 순1,
+--    DENSE_RANK() OVER (ORDER BY height DESC) 순2,
+    grade,
+    RANK() OVER (PARTITION BY grade ORDER BY height DESC) 순3,
+    DENSE_RANK() OVER (PARTITION BY grade ORDER BY height DESC) 순4
+FROM student
+ORDER BY grade, height desc;
 
