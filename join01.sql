502987304cc6ed758250e9dd67f1e9adde3a3912
PCJ-93 <pw2900@naver.com>
2024-11-07 16:52:12 +0900

    셀프조인

--- join01.sql
+++ join01.sql
@@ -236,14 +236,94 @@ SELECT
     SUM(p_total) 판매금액
 FROM panmae
 GROUP BY ROLLUP( p_date, p_code );
-
 -- 퀴즈 끝 --
 
+--SELF JOIN -- 같은 테이블이지만 두개의 테이블이라고 생각해서 사용하면 편할수도있다.
+
+-- 내 사번, 내이름 , 내상사의사번 , 상사의이름
+SELECT 
+    e1.empno 내사번,
+    e1.ename 내이름,
+    e1.mgr 내상사의사번,
+    e2.empno 상사의사번,
+    e2.ename 상사의이름
+FROM emp e1, emp e2
+WHERE e1.mgr = e2.empno;
+
+-- 간단한 연습 --
+SELECT
+    s.name "STU_NAME",
+    s.deptno1,
+    d.dname "DEPT_NAME"
+FROM student s, department d
+WHERE s.deptno1 = d.deptno
+ORDER BY deptno1;
+
+SELECT
+    e.name,
+    e.position,
+    TO_CHAR(e.pay,'999,999,999') "PAY",
+    TO_CHAR(p.s_pay,'999,999,999') "LOW PAY",
+    TO_CHAR(p.e_pay,'999,999,999') "HIGH PAY"
+FROM emp2 e, p_grade p
+WHERE e.position = p.position(+) AND e.position is not null
+ORDER BY s_pay;
+
+SELECT
+    e.name NAME,
+    TO_CHAR(TO_DATE('2010-05-30'),'YYYY') - TO_CHAR(e.birthday,'YYYY')+1 한국나이,
+    --2010-TO_CHAR(e.birthday,'YYYY')+1 한국나이,
+    e.position 지금포지션,
+    p.position 그나이에맞는포지션
+FROM emp2 e, p_grade p
+WHERE TO_CHAR(TO_DATE('2010-05-30'),'YYYY') - TO_CHAR(e.birthday,'YYYY')+1 BETWEEN p.s_age AND p.e_age;
 
 
+SELECT 
+    c.gname,
+    c.point,
+    g.gname
+FROM customer c, gift g
+WHERE c.point >= g.g_start
+AND g.gno = 7;
+  --g.gname = 'Notebook';
+--GROUP BY c.gname, point, g.gname
+--HAVING g.gname = 'Notebook';
 
 
+SELECT
+    profno,
+    name,
+    hiredate,
+    RANK() OVER (ORDER BY hiredate)-1
+FROM professor
+ORDER BY hiredate;
 
 
+SELECT 
+    p1.profno,
+    p1.name,
+    p1.hiredate,
+    COUNT(p2.profno) COUNT
+FROM professor p1, professor p2
+WHERE p1.hiredate > p2.hiredate(+)
+GROUP BY p1.profno, p1.name, p1.hiredate
+ORDER BY p1.hiredate;
 
 
+SELECT
+    empno,
+    ename,
+    hiredate,
+    RANK() OVER (ORDER BY hiredate)-1 COUNT
+FROM emp;
+
+SELECT
+    e1.empno,
+    e1.ename,
+    e1.hiredate,
+    COUNT(e2.empno) COUNT
+FROM emp e1, emp e2
+WHERE e1.hiredate > e2.hiredate(+)
+GROUP BY e1.empno, e1.ename, e1.hiredate
+ORDER BY e1.hiredate;
