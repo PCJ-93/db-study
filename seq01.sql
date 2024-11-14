bb28c1d41d8676f0e1944741fbda7527c7422f49
PCJ-93 <pw2900@naver.com>
2024-11-14 10:35:46 +0900

    시퀀스 모닝퀴즈

--- seq01.sql
+++ seq01.sql
@@ -164,16 +164,80 @@ SELECT * FROM T_ITEM_LIST;
 -- 연습 끝 --
 
 
+-- 모닝 퀴즈 --
+--1. 다음 두 명령어는 어떤 기능을 수행하는 명령어인지 작성하고,
+--두 기능의 차이점이 있다면 설명하시오.
+DELETE FROM 테이블명; DML 테이블안의 데이터를 삭제 하는 명령어 -차이점 WHERE을 넣어서 선택해서 삭제할수있다 커밋롤백가능
+TRUNCATE TABLE 테이블명; DDL 테이블안의 모든 데이터를 삭제하는 명령어 롤백X 자동커밋
 
+--2. 다음 조건에 따라 테이블을 생성하시오.
+--테이블명 : T_MEMBER_POINT
+--
+--*내부 컬럼
+--ID : 숫자형 6자리
+--순번 : 숫자형 6자리
+--멤버ID : 문자형 24바이트, Null 안됨(꼭 입력해야함)
+--점수 : 숫자형 3자리
+--채점일시 : 날짜형, 단 입력된 값이 없을 경우 입력(현재)시간을 기본값으로 설정
+--※ 기본키(PK) : ID와 순번의 조합
 
+CREATE TABLE T_MEMBER_POINT
+(
+    id NUMBER(6),
+    count NUMBER(6),
+    m_id VARCHAR2(24) NOT NULL,
+    score NUMBER(3),
+    check_date DATE DEFAULT SYSDATE,
+    CONSTRAINT T_MEMBER_POINT_PK PRIMARY KEY(id,count)
+);
+--TRUNCATE TABLE T_MEMBER_POINT;
 
+--3. 다음 조건에 맞는 시퀀스를 생성하시오. -> 생성한 시퀀스는 id 컬럼에 사용
+--시퀀스명 : T_MEMBER_POINT_PK_SEQ
+--*상세조건
+--1부터 시작하며 1씩 증가한다.
+--값의 범위는 1~999999
+--순환하지 않도록 한다.
+CREATE SEQUENCE T_MEMBER_POINT_PK_SEQ
+INCREMENT BY 1 
+START WITH 1
+MINVALUE 1
+MAXVALUE 999999
+NOCYCLE;
+--DROP SEQUENCE T_MEMBER_POINT_PK_SEQ;
 
+--4. 생성한 시퀀스의 값을 불러서 임의의 데이터를 저장해보시오.
+--(*순번은 각 멤버ID 별로 자동 계산되어 저장하도록 해주세요)
+--- 시퀀스 사용 -> id
+--- 순번(no) -> 해당 학생이 시험을 치룬 회차 계산되서 저장
+SELECT T_MEMBER_POINT_PK_SEQ.nextval FROM dual;
+SELECT T_MEMBER_POINT_PK_SEQ.currval FROM dual;
+SELECT * FROM  T_MEMBER_POINT;
 
+INSERT INTO T_MEMBER_POINT
+VALUES ( ( T_MEMBER_POINT_PK_SEQ.nextval ), (SELECT COUNT(*)+1
+                                                FROM t_member_point
+                                                WHERE m_id = '1번') ,'1번', 65, '2015/05/03');
+INSERT INTO T_MEMBER_POINT
+VALUES ( ( T_MEMBER_POINT_PK_SEQ.nextval ), (SELECT COUNT(*)+1
+                                                FROM t_member_point
+                                                WHERE m_id = '2번') ,'2번', 75, '2015/05/08');
+INSERT INTO T_MEMBER_POINT
+VALUES ( ( T_MEMBER_POINT_PK_SEQ.nextval ), (SELECT NVL(MAX(m_id),0)+1
+                                                FROM t_member_point
+                                                WHERE m_id = '3번') ,'3번', 79, '2016/05/02');
+INSERT INTO T_MEMBER_POINT
+VALUES ( ( T_MEMBER_POINT_PK_SEQ.nextval ), (SELECT NVL(MAX(m_id),0)+1
+                                                FROM t_member_point
+                                                WHERE m_id = '4번') ,'4번', 75, SYSDATE);
+SELECT COUNT(*)+1
+FROM t_member_point  -- m_id올리는 1번째 방법
+WHERE m_id = 'n번';
 
-
-
-
-
+SELECT NVL(MAX(m_id),0)+1
+FROM t_member_point  -- m_id올리는 2번째 방법
+WHERE m_id = 'n번';
+-- 모닝 퀴즈 끝 --
 
 
 
